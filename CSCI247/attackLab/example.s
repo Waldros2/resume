15a1ec38a7461c300fcac5e874036d82b823c320
Scott Waldron <waldros2@gmail.com>
2017-11-01 14:27:08 -0700

    Removed uneccesary files

--- CSCI247/attackLab/example.s
+++
@@ -1,6 +0,0 @@
-# Example of hand-generated assembly code
-popq 	%rax  				#Push value onto stack	
-movq	%rax,%rdi	                #Move  into %rdi
-movq    $0x00401a74,(%rsp)		#Move into %rsp
-retq 
-
